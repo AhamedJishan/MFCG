14c1f92179238daf8ecd2a40b7398b4342091421
Jishan Ahamed <jjahamed33@gmail.com>
2024-06-28 01:32:10 +0530

    Vec3 added

--- MFCG/include/Vec3.h
+++ MFCG/include/Vec3.h
@@ -1,1 +1,19 @@
 #pragma once
+
+namespace MFCG
+{
+	template <typename T>
+	class Vec3
+	{
+	public:
+
+		Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
+		Vec3(const T& t) : x(t), y(t), z(t) {}
+		Vec3(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
+
+	public:
+		T x, y, z;
+	};
+
+	typedef Vec3<float> Vec3f;
+}
---
+++ MFCG/src/Main.cpp
@@ -0,0 +1,6 @@
+#include "Vec3.h"
+
+int main()
+{
+	MFCG::Vec3f a;
+}
